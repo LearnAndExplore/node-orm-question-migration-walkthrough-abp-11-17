22f2d0ea3d5b3c85262464db6331969b963c5619
LearnAndExplore <pangpang_1993_0506@yahoo.com.hk>
2017-12-02 07:53:17 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -7,7 +7,7 @@ class Question{
     return new Promise (
       function(resolve){
         console.log("I am running")
-        db.run("CREATE TABLE questions(id INTEGER PRIMARY KEY, questions TEXT)",
+        db.run("CREATE TABLE questions(id INTEGER PRIMARY KEY, content TEXT)",
         function(){resolve("ABC")
       })
       console.log("I finished running")
